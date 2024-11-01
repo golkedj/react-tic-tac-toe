1ee9e4a6960adad41ecddeded5cdbd3ec0e6f4ae
David Golke <golkedj@gmail.com>
2024-11-01 02:31:45 -0500

    Cleanup

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,17 +1,4 @@
 export default function GameBoard({ onSelectSquare, board }) {
-  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  //   function handleSelectSquare(rowIndex, colIndex) {
-  //     setGameBoard((prevGameBoard) => {
-  //       const updatedBoard = [...prevGameBoard.map((row) => [...row])];
-  //       updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //       console.log(updatedBoard);
-  //       return updatedBoard;
-  //     });
-
-  //     onSelectSquare();
-  //   }
-
   return (
     <ol id="game-board">
       {board.map((row, rowIndex) => (
